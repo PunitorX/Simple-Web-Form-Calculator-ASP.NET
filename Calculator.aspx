5724e2927fdb7a7cc76a99f6a7d439cf87b63a3d
Robert Jones <jones.brendan1996@gmail.com>
2022-05-01 19:31:00 -0600

    Inital Commit

---
+++ Calculator.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Calculator.aspx.cs" Inherits="WebApplicationAssignment.Calculator" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml" style="background-color: #FFFFFF; font-size: medium; color: #000000">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body style="position: relative">
+    <form id="form1" runat="server">
+        <div style="text-align: left; font-weight: 700">
+            <h1 style="text-align: center">Calculator</h1>
+        </div>
+        <p style="text-align: center">
+&nbsp;First Number:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="firstNum" runat="server"></asp:TextBox>
+        </p>
+        <p style="text-align: center">
+&nbsp;&nbsp;&nbsp;&nbsp; Second Number:&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="secondNum" runat="server"></asp:TextBox>
+        </p>
+        <div class="auto-style1">
+            <asp:Button ID="addButton" runat="server" Text="Add" OnClick="addButton_Click" />
+            <asp:Button ID="subButton" runat="server" Text="Subtract" OnClick="subButton_Click" />
+            <asp:Button ID="multiButton" runat="server" Text="Multiply" OnClick="multiButton_Click" />
+            <asp:Button ID="divButtin" runat="server" Text="Divide" OnClick="divButtin_Click" />
+            <asp:Button ID="modButtun" runat="server" Text="Modulus" OnClick="modButtun_Click" />
+        </div>
+        <p style="text-align: center">
+            <asp:TextBox ID="resultNum" runat="server"></asp:TextBox>
+        &nbsp;
+            <asp:Button ID="clearButton" runat="server" OnClick="clearButton_Click" Text="Clear" />
+        </p>
+    </form>
+</body>
+</html>
---
+++ Calculator.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationAssignment
+{
+    public partial class Calculator : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void addButton_Click(object sender, EventArgs e) // Add button on web page
+        {
+            try
+            {
+                // Checks to see if there are numerals in the boxes.
+                if (firstNum.Text != "" && secondNum.Text != "")
+                {
+                    resultNum.Text = (double.Parse(firstNum.Text.ToString()) + double.Parse(secondNum.Text.ToString())).ToString();
+                }
+            }
+            catch(FormatException) // This catches and proclaims that the user must enter only numerals
+            {
+                resultNum.Text = "Must be a number!"; // Displayed string if the catch is executed.
+            }
+        }
+
+        protected void subButton_Click(object sender, EventArgs e) // Subtract button on web page
+        {
+            try
+            {
+                // Checks to see if there are numerals in the boxes.
+                if (firstNum.Text != "" && secondNum.Text != "")
+                {
+                    resultNum.Text = (double.Parse(firstNum.Text.ToString()) - double.Parse(secondNum.Text.ToString())).ToString();
+                }
+            }
+            catch (FormatException) // This catches and proclaims that the user must enter only numerals
+            {
+                resultNum.Text = "Must be a number!"; // Displayed string if the catch is executed.
+            }
+        }
+
+        protected void multiButton_Click(object sender, EventArgs e) // Multiply button on web page
+        {
+            try
+            {
+                // Checks to see if there are numerals in the boxes.
+                if (firstNum.Text != "" && secondNum.Text != "")
+                {
+                    resultNum.Text = (double.Parse(firstNum.Text.ToString()) * double.Parse(secondNum.Text.ToString())).ToString();
+                }
+            }
+            catch (FormatException) // This catches and proclaims that the user must enter only numerals
+            {
+                resultNum.Text = "Must be a number!"; // Displayed string if the catch is executed.
+            }
+        }
+
+        protected void divButtin_Click(object sender, EventArgs e) // Divide button on web page
+        {
+            try
+            {
+                // Checks to see if there are numerals in the boxes.
+                if (firstNum.Text != "" && secondNum.Text != "")
+                {
+                    resultNum.Text = (double.Parse(firstNum.Text.ToString()) / double.Parse(secondNum.Text.ToString())).ToString();
+                   
+                    if (secondNum.Text == "0") // If the second number text box (denominator) is zero, the following string is displayed.
+                    {
+                        resultNum.Text = "Can not divide by zero!"; // Displayed if the second number (denominator) is zero
+                    }
+                }
+            }           
+            catch (FormatException) // This catches and proclaims that the user must enter only numerals
+            {
+                resultNum.Text = "Must be a number!"; // Displayed string if the catch is executed.
+            }           
+        }
+
+        protected void modButtun_Click(object sender, EventArgs e) // Modulus button on web page
+        {
+            try
+            {
+                // Checks to see if there are numerals in the boxes.
+                if (firstNum.Text != "" && secondNum.Text != "")
+                {
+                    resultNum.Text = (double.Parse(firstNum.Text.ToString()) % double.Parse(secondNum.Text.ToString())).ToString();
+                }
+            }
+            catch (FormatException) // This catches and proclaims that the user must enter only numerals
+            {
+                resultNum.Text = "Must be a number!"; // Displayed string if the catch is executed.
+            }
+        }
+
+        protected void clearButton_Click(object sender, EventArgs e)
+        {
+            firstNum.Text = ""; // sets the number one textbox to null
+            secondNum.Text = ""; // sets the number two textbox to null
+            resultNum.Text = ""; // sets the result textbox to null
+        }
+    }
+}
